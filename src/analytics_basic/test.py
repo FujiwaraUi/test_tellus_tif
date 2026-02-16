34db1981770d321c678e0e084ea4d35d21517318
Yui Fujiwara <r.waremokou@gmail.com>
2026-02-16 19:45:07 +0900

    implemention unet

--- src/analytics_basic/test.py
+++ src/analytics_basic/test.py
@@ -3,6 +3,8 @@ import timm
 import os
 import torch
 import tifffile as tif
+import segmentation_models_pytorch as smp
+from IPython.display import display
 from torchview import draw_graph
 
 # IPython があれば display を使う。なければダミーにする。
@@ -13,7 +15,7 @@ except ModuleNotFoundError:
         return  # スクリプト実行では何もしない
 
 
-def prac_cnn(hyper_cube, outdir="./data"):
+def prac_resnet(hyper_cube, outdir="./data"):
     H, W, B = hyper_cube.shape
 
     x = hyper_cube.astype(np.float32, copy=False)
@@ -42,9 +44,44 @@ def prac_cnn(hyper_cube, outdir="./data"):
         cleanup=True,
     )
     
+def prac_unet(hyper_cube, outdir="./data"):
+    H, W, B = hyper_cube.shape
 
+    x = hyper_cube.astype(np.float32, copy=False)
+    x = (x - x.min()) / (x.max() - x.min() + 1e-8)
+
+    inputs = torch.from_numpy(x).permute(2, 0, 1).unsqueeze(0)
+
+    
+    unet = smp.Unet(
+        encoder_name="resnet18",
+        encoder_weights=None,
+        in_channels=B,
+        classes=10,
+    )
+    print(unet)
+    
+    outputs = unet(inputs)[0]
+    print(f"\n Model Output Class: {outputs.shape}")
+    
+    model_graph = draw_graph(unet, input_size=tuple(inputs.shape), expand_nested=True)
+    display(model_graph.visual_graph)
+    
+    model_graph.visual_graph.graph_attr["dpi"] = "1800"  # 例: 300, 600, 1200 など
+    os.makedirs(outdir, exist_ok=True)
+    model_graph.visual_graph.render(
+        filename=os.path.join(outdir, "UNet_Graph"),
+        format="png",
+        cleanup=True,
+    )
+    
+    
+    
 if __name__ == "__main__":
     filepath = "/Volumes/ssd/HSID/HISUI/HSHL1G_N329E1299_20230523072720_20240308144532.tif"
     outdir = "./data"
     hyper_cube = tif.imread(filepath)
-    prac_cnn(hyper_cube, outdir=outdir)
+    
+    # prac_resnet(hyper_cube, outdir=outdir)
+
+    prac_unet(hyper_cube, outdir=outdir)
